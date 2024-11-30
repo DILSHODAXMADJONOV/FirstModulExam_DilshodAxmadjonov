24bf9867acc2a1f5b24d8c7db5f68e0fc20aecef
Your Name <you@example.com>
2024-11-30 10:14:19 +0500

    exam

---
+++ Exam/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        //1
+        public static int CountElemnt(List<int> numberElements)
+        {
+            var count = 0;
+            foreach (var numberElement in numberElements)
+            {
+                if (numberElement > 10 && numberElement < 100)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        //2
+        public static string TextSecontDelete(string text)
+        {
+            text = text.Substring(0, text.Length - 2);
+            return text;
+        }
+        //3
+        public static bool Check(string texts)
+        {
+            foreach (var text in texts)
+            {
+                if (char.IsLower(text))
+                {
+                    return false;
+                    break;
+                }
+            }
+            return true;
+        }
+        //4
+        public static bool CheckText(string text)
+        {
+            if (text.StartsWith("qovun") && text.EndsWith("qovun") is true)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
---
+++ Exam/Services/PhoneService.cs
@@ -0,0 +1,68 @@
+﻿using Exam.Models;
+
+namespace Exam.Services;
+
+public class PhoneService
+{
+    private List<Phone> phones;
+    public PhoneService()
+    {
+        phones = new List<Phone>();
+    }
+
+    public Phone AddPhone(Phone restaurant)
+    {
+        restaurant.Id = Guid.NewGuid();
+        phones.Add(restaurant);
+        return restaurant;
+    }
+
+    public bool DeletePhone(Guid phoneId)
+    {
+        var exists = false;
+        foreach (var phone in phones)
+        {
+            if (phone.Id == phoneId)
+            {
+                phones.Remove(phone);
+                exists = true;
+                break;
+            }
+        }
+
+        return exists;
+    }
+
+    public bool UpdatePhone(Phone updatedRestaurant)
+    {
+        for (var i = 0; i < phones.Count; i++)
+        {
+            if (phones[i].Id == updatedRestaurant.Id)
+            {
+                phones[i] = updatedRestaurant;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public Phone GetById(Guid phoneId)
+    {
+        foreach (var phone in phones)
+        {
+            if (phone.Id == phoneId)
+            {
+                return phone;
+            }
+        }
+
+        return null;
+    }
+
+    public List<Phone> GetAllPhones()
+    {
+        return phones;
+    }
+}
+
